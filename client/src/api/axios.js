ef4b41070f0b7494930b9a4301b5ab7bbb74f749
krishx06 <krishpatil120@gmail.com>
2026-02-07 17:50:57 +0530

    refactor(client): update auth utilities and add new styles

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,9 +3,6 @@ import { getToken, removeToken } from "../utils/auth";
 
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 api.interceptors.request.use(
